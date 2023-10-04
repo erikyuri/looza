a65c63b43ffecffd778ac48bc2bb3a6ce096c250
erikyuri <erikyurisf@gmail.com>
2023-10-04 20:01:42 -0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: () => import('../app/Pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
